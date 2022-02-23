a8f5fae81f72449e7cb27d78cb81f4e962253501
Keanin Cupido <keanin16@gmail.com>
2022-02-23 15:42:44 +0200

    style: featured projects grid

--- kcc_portfolio/src/App.js
+++ kcc_portfolio/src/App.js
@@ -1,8 +1,12 @@
-import { Container, Flex, Heading, Link, Text, VStack } from "@chakra-ui/react";
+import { Flex, Grid, Heading, Link, Text, VStack } from "@chakra-ui/react";
 import { FaArrowRight } from "react-icons/fa";
 import { useColorMode } from '@chakra-ui/color-mode'
 
 import Navbar from "./components/Navbar";
+import ProjectCard from "./components/ProjectCard/ProjectCard";
+
+// Project Images
+import project_img_01 from './components/ProjectCard/img/project_img.jpg';
 
 function App() {
 
@@ -10,16 +14,17 @@ function App() {
   const isDark = colorMode === 'dark';
 
   return (
-      <VStack>
+      <VStack w="100%">
         <Navbar />
 
-        <VStack p="5em 15em 5em 15em" justify="left" align="left" spacing="2em">
+        <VStack w="100%" p="5em 15em 5em 15em" justify="left" align="left" spacing="2em">
           <Flex direction="column" gridGap="1em">
             <Text>Hey, I'm</Text>
             <Heading fontWeight="700" fontStyle="normal" fontSize="6rem" bgGradient='linear-gradient(90deg, #9845E8 0%, #33D2FF 20%, #DD5789 50%)' bgClip='text'>Keanin Cupido</Heading>
           </Flex>
+          
           <Flex direction="column" gridGap="1.5em">
-            <Text  color={isDark ? "#F0F0F0" : "#1A1A1A" } fontWeight="200" fontStyle="normal" fontSize="1.125rem">
+            <Text color={isDark ? "#F0F0F0" : "#1A1A1A" } fontWeight="200" fontStyle="normal" fontSize="1.125rem">
               I'm a front-end developer based in Cape Town, South Africa and also a Computer Science student. 
               I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. 
               I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.
@@ -35,14 +40,49 @@ function App() {
           </Flex>
         </VStack>
 
-        {/* Projects Section */}
-        <VStack p="2.5em 15em 5em 15em" justify="left" align="left" spacing="1em">
+        {/* Featured Projects */}
+        <Flex direction="column" p="0em 15em 5em 15em" align='flex-start' w="100%" alignItems='flex-start' verticalAlign='top' spacing="1em">
           <Text fontWeight="300" fontStyle="normal" fontSize="1.125rem">FEATURED PROJECTS</Text>
-          <Container p={0} justify="left" align="left">
+          <Grid pt="2em" justify="left" align="left" templateColumns='repeat(3, 1fr)' gap="3em">
             {/* Project Card Components */}
-            <Text fontWeight="300" fontStyle="normal" fontSize="1.125rem">TEST TEXT</Text>
-          </Container>
-        </VStack>
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 1"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 2"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 3"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 4"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 5"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+              <ProjectCard
+                project_img={project_img_01}
+                project_name="Project 6"
+                project_desc="Proxy provider website including authentication, dashboard and dynamic features."
+                project_tech="React - Bootstrap - Styled Components"
+              />
+          </Grid>
+        </Flex>
       </VStack>
   );
 }
---
+++ kcc_portfolio/src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,28 @@
+import { Container, Image, Text, Flex, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+import { FaGithub, FaLink } from 'react-icons/fa';
+
+function ProjectCard(props) {
+
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === 'dark';
+
+  return (
+    <Container p={0} maxW='400px'>
+        <Flex direction='column' justify="flex-start" align="flex-start" gridGap="1em">
+          <Image src={props.project_img} borderRadius="4px"/>
+          <Flex width="100%" direction="row" justify="space-between" align="space-between">
+            <Text fontWeight="300" fontStyle="normal" fontSize="1.5rem">{props.project_name}</Text>
+            <Flex direction="row" justify="right" align="center" gridGap="1em">
+                <FaGithub />
+                <FaLink />
+            </Flex>
+          </Flex>
+          <Text fontWeight="300" fontStyle="normal" fontSize="1rem"  color={isDark ? "#C4C4C4" : "#1A1A1A" } lineHeight="1.5em">{props.project_desc}</Text>
+          <Text fontWeight="300" fontStyle="normal" fontSize="0.875rem"  color="#6CACE4">{props.project_tech}</Text>
+        </Flex>
+    </Container>
+  )
+}
+
+export default ProjectCard
